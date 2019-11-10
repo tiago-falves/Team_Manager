08911564472138b07cde1fcd0c3377f16a0175c6
TsarkFC <45267600+TsarkFC@users.noreply.github.com>
2019-11-10 18:58:20 +0000

    Implemented read from CallUp.txt and manage games inside CallUp methods

--- CallUp.cpp
+++ CallUp.cpp
@@ -47,3 +47,56 @@ void CallUp::setPlayerStatistics(const vector<CallUpPlayerStatistics *> playerSt
     this->playerStatistics = playerStatistics;
 }
 
+//CHANGE CALLUP
+void CallUp::changeDailyCost(float dailyCost) {
+    this->dailyCost = dailyCost;
+}
+
+void CallUp::changeBeginDate(Date beg) {
+    this->begginingDate = beg;
+}
+
+void CallUp::changeEndDate(Date end) {
+    this->endDate = end;
+}
+
+//ADD / REMOVE GAME
+void CallUp::addGame(Game *game) {
+    //if (!gameExists(game)) //throw exception game already exists
+    insert_sorted(games, game);
+}
+void CallUp::removeGame(Game *game){
+    games.erase(games.begin() + BinarySearch(games, game));
+}
+
+//ADD / REMOVE PLAYER
+void CallUp::addPlayer(int playerID, Date begDate, Date endDate) {
+    //if(playerExists(playerID)) //throw exception player already exists
+    CallUpPlayerStatistics player(playerID, begDate, endDate);
+    insert_sorted(playerStatistics, &player);
+}
+void CallUp::removePlayer(int playerID) {
+    //FIND PLAYER POSITION
+    vector<CallUpPlayerStatistics*>::const_iterator it;
+    for (auto i = playerStatistics.begin(); i != playerStatistics.end(); i++){
+        if ((*i)->getPlayerID() == playerID) {
+            it = i;
+            break;
+        }
+    }
+    playerStatistics.erase(it);
+}
+
+
+
+//UTILITIES
+bool CallUp::gameExists(Game *game) {
+    if (BinarySearch(games, game) == -1) return false;
+    return true;
+}
+bool CallUp::playerExists(int playerID) {
+    for (auto i = playerStatistics.begin(); i != playerStatistics.end(); i++){
+        if ((*i)->getPlayerID() == playerID) return true;
+    }
+    return false;
+}
